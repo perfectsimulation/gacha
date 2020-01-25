ced474fc9c335cd3df6d6ca18d3db7b1fb85256c
Sally Blair <sally@sallyblair.io>
2020-01-25 14:46:57 -0800

    Stub main and adventure scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Open the Adventure scene from the Main scene
+    public void StartAdventure()
+    {
+        // called onClick PlayButton in Main scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageManager : MonoBehaviour
+{
+    // Open a Stage scene from the Adventure scene
+    public void StartStage()
+    {
+        // TODO: add argument for stage details
+        // called onClick StageButton in Adventure menu
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
