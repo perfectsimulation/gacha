a66a8e668e879488ab1ffb3d8ea30041e06f6944
Sally Blair <sally@sallyblair.io>
2020-07-06 15:57:22 -0700

    Add to gitignore the local save data

--- Assets/Scripts/Card/CardData.cs
+++ Assets/Scripts/Card/CardData.cs
@@ -55,58 +55,85 @@ public class CardData
         this.severity = severity;
     }
 
+    // Increments card experience and checks if card gained enough experience to level up
     public void IncrementExperience(float delta)
     {
         this.experience += delta;
         this.CheckForLevelUp();
     }
 
+    // Checks for level up by comparing current experience against required experience
     private void CheckForLevelUp()
     {
+        // Get the theoretical card level based on current experience
         int levelForExperience = this.GetLevelFromExperience(this.experience);
+        // Compare current card level with theoretical card level
         if (this.level < levelForExperience)
         {
             // Level up time! TODO celebrate good times
-            this.level = levelForExperience;
+            this.LevelUp();
+            // Check if card has gained enough levels to rank up
             this.CheckForRankUp();
         }
     }
 
+    // Increments current level by one
+    private void LevelUp()
+    {
+        this.level++;
+    }
+
+    // Checks for rank up by comparing current level against required level
     private void CheckForRankUp()
     {
+        // Get the theoretical card rank based on current level
         int rankForLevel = this.GetRankFromLevel(this.level);
         if (this.rank < rankForLevel)
         {
-            // Rank up time! TODO come on!
-            this.rank = rankForLevel;
+            // Rank up time! TODO check item requirements
+            this.RankUp();
         }
     }
 
+    // Increments current rank by one
+    private void RankUp()
+    {
+        this.rank++;
+    }
+
+    // Returns theoretical level by checking level requirements
     private int GetLevelFromExperience(float experience)
     {
         // Go through level requirements starting from the highest requirement
         for (int i = this.levelReqs.Length - 1; i >= 0; i--)
         {
+            // If experience exceeds the required experience of this level
             if (experience > this.levelReqs[i].experience)
             {
+                // This is the first level we exceeded, and we started from the top
                 return this.levelReqs[i].level;
             }
         }
 
+        // Should never execute
         return 1;
     }
 
+    // Returns theoretical rank by checking rank requirements
     private int GetRankFromLevel(int level)
     {
         // Go through rank requirements starting from the highest requirement
         for (int i = this.rankReqs.Length - 1; i >= 0; i--)
         {
+            // If level exceeds the required level of this rank
             if (level > this.rankReqs[i].level)
             {
-                return this.rankReqs[i].level;
+                // This is the first rank we exceeded, and we started from the top
+                return this.rankReqs[i].rank;
             }
         }
 
+        // Should never execute
         return 1;
     }
 }
@@ -119,10 +146,11 @@ public class LevelRequirement
 
     public LevelRequirement()
     {
-        this.level = 2;
-        this.experience = 100f;
+        this.level = 0;
+        this.experience = 0f;
     }
 
+    // Experience required for this level
     public LevelRequirement(int level, float experience)
     {
         this.level = level;
@@ -139,11 +167,12 @@ public class RankRequirement
 
     public RankRequirement()
     {
-        this.rank = 1;
-        this.level = 1;
+        this.rank = 0;
+        this.level = 0;
         this.items = new List<ItemData>();
     }
 
+    // Level and items required for this rank
     public RankRequirement(int rank, int level, List<ItemData> items)
     {
         this.rank = rank;
